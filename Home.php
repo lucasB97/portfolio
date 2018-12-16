8f96e9e9a13f12a9f2a5fc8a963c6bc08985dbdc
lucasB97 <lucasbessa708@gmail.com>
2018-12-16 19:54:51 -0300

    primeiro portfólio

---
+++ Home.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lucas</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet"  href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet"  href="bootstrap/css/bootstrap-theme.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="booststrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<script language="JavaScript" src="js/navbar.js"></script>
+</head>
+<body>
+	<nav id="header" class="navbar navbar-fixed-top">
+		<div id="header-container" class="container navbar-container">
+			<div class="navbar-header">
+				<a id="brand" class="navbar-brand" href="#">Lucas Bessa</a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="#Sobre">Sobre</a></li>
+					<li><a href="#Habilidades">Habilidades</a></li>
+					<li><a href="#Formação">Formação</a></li>
+					<li><a href="#Contato">Contato</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div id="Sobre" class="container-fluid " style="padding-top:70px;padding-bottom:200px;">
+		<h1>Sobre</h1><br>
+		<p align="center"><font size="5">Me chamo Lucas, tenho 21 anos e sou desenvolvedor web disposto a novos desafios e a procura de uma primeira experiência na área.Já desenvolvi alguns projetos pequenos em grupo,tanto em PHP quanto em java,atuando na parte de front e back-end. 
+			</font>
+		</p>
+	</div>
+	<div id="Habilidades" class="container-fluid " style="padding-top:70px;padding-bottom:70px; background-color: #B0C4DE">
+		<h1>Habilidades</h1><br>
+		<div class="container1">
+			<img src="img/bootstrap.png"  class="img-fluid" width="150">
+			<img src="img/mysql.png"  class="img-fluid">
+			<img src="img/logo-html-css.png"  class="img-fluid">
+			<img src="img/480px-Unofficial_JavaScript_logo_2.svg.png" class="img-fluid" style="width: 80px">
+			<img src="img/logo-java.png"  class="img-fluid">
+			<img src="img/php_PNG38.png"  class="img-fluid">
+		</div>
+		<div class="container2">
+			<h1>Familiarizado</h1><br>
+			<img src="img/git.png"  class="img-fluid">
+			<img src="img/Octocat.png"  class="img-fluid">
+			<img src="img/xampp.png"  class="img-fluid">
+		</div>
+	</div>
+	<div id="Formação" class="container-fluid " style="padding-top:70px;padding-bottom:70px">
+		<h1 align="center">Formação</h1><br>
+		<div class="container3">
+		<p align="center"><img src="img/ifpe.png"  class="img-fluid"></p><br>
+		<p align="center"><font size="5">Sou recem formado pelo Instituto Federal de Pernambuco - campus Jaboatão
+		</font>
+		</p>
+		</div>
+	</div>
+	<div id="Contato" class="container-fluid " style="padding-top:70px;padding-bottom:20px; background-color:#696969;">
+		<h1><font color="black"> Contato </font> </h1>
+		<div class="dados">
+			<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span><p><font size="5" color="black">lucasbessa708@gmail.com</p></font><br>
+			<span class="glyphicon glyphicon-earphone" aria-hidden="true"></span><p><font size="5" color="black">(81)994298605</font></p><br>
+			<span class="glyphicon glyphicon-file" aria-hidden="true"></span>
+			<a href="cv/cv.pdf" target="_blank"><p><font size="5">
+			Currículo </p></font></a>
+		</div>
+		<div>
+		<ul class="soc">
+		   <li><a href="https://github.com/lucasB97" class="icon-13 github" title="GitHub" target="_blank"><svg viewBox="0 0 512 512"><path d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z"/></svg><!--[if lt IE 9]><em>GitHub</em><![endif]--></a>
+		   	</li>
+		   	  <li><a href="https://www.linkedin.com/in/lucas-bessa-a8a009132/" class="icon-17 linkedin" title="LinkedIn" target="_blank"><svg viewBox="0 0 512 512"><path d="M186.4 142.4c0 19-15.3 34.5-34.2 34.5 -18.9 0-34.2-15.4-34.2-34.5 0-19 15.3-34.5 34.2-34.5C171.1 107.9 186.4 123.4 186.4 142.4zM181.4 201.3h-57.8V388.1h57.8V201.3zM273.8 201.3h-55.4V388.1h55.4c0 0 0-69.3 0-98 0-26.3 12.1-41.9 35.2-41.9 21.3 0 31.5 15 31.5 41.9 0 26.9 0 98 0 98h57.5c0 0 0-68.2 0-118.3 0-50-28.3-74.2-68-74.2 -39.6 0-56.3 30.9-56.3 30.9v-25.2H273.8z"/></svg><!--[if lt IE 9]><em>LinkedIn</em><![endif]--></a></li>
+		</ul>
+	</div>
+
+</body>
+
+</html>
